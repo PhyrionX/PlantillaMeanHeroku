538cb99052a90e3abb8caf0dc7d43bd1a068fce9
phyrionX <rgcmb@hotmail.com>
2016-02-22 22:06:45 +0100

    Pequeños retoques para angular

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(compression());
 }));*/
 app.use(methodOverride());
 
-app.use(express.static(path.join(__dirname, './client/staticWeb')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.set('dbUrl', process.env.BBDD || config.db.test);
  // connect mongoose to the mongo dbUrl
